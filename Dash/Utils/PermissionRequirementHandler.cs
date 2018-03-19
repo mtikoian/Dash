ed8d0c1d32823d18d19af50c0c1e89e00e9cd526
Chris Pittman <cpittman@gmail.com>
2018-03-19 19:42:48 -0400

    Implement parentAction attribute.

--- Dash/Utils/PermissionRequirementHandler.cs
+++ Dash/Utils/PermissionRequirementHandler.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace Dash.Utils
 {
@@ -9,9 +12,19 @@ namespace Dash.Utils
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
         {
-            // @todo add support for ParentActionAttribute
             var mvcContext = context.Resource as AuthorizationFilterContext;
             var requiredClaim = $"{mvcContext.RouteData.Values["Controller"]}.{mvcContext.RouteData.Values["Action"]}".ToLower();
+            var controllerActionDescriptor = mvcContext.ActionDescriptor as ControllerActionDescriptor;
+            if (controllerActionDescriptor != null)
+            {
+                var actionAttributes = controllerActionDescriptor.MethodInfo.GetCustomAttributes(inherit: true).Cast<Attribute>();
+                var parentAction = actionAttributes.FirstOrDefault(x => x.GetType() == typeof(ParentActionAttribute));
+                if (parentAction != null)
+                {
+                    requiredClaim = $"{mvcContext.RouteData.Values["Controller"]}.{((ParentActionAttribute)parentAction).ActionName}".ToLower();
+                }
+            }
+
             if (context.User.HasClaim(x => x.Type == ClaimTypes.Role && x.Value.ToLower() == requiredClaim))
             {
                 context.Succeed(requirement);
