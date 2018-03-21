ba6a6f62d4e7ca944b82a69abef81b17a67b43d4
Chris Pittman <cpittman@gmail.com>
2018-03-21 18:45:16 -0400

    Remove unneeded comment.

--- Dash/Models/Permission.cs
+++ Dash/Models/Permission.cs
@@ -1,8 +1,5 @@
 ﻿namespace Dash.Models
 {
-    /// <summary>
-    /// Permission is a combination of action and controller, used for user authorization. AuthActionFilter checks permissions.
-    /// </summary>
     public class Permission : BaseModel
     {
         public string ActionName { get; set; }
