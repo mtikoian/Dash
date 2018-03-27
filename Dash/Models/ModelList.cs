08f86c39ed2c64139bec19582b34c6a1e15ac31a
Chris Pittman <cpittman@gmail.com>
2018-03-26 20:23:02 -0400

    Troubleshooting issues with deserializing models.

---
+++ Dash/Models/ModelList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dash.Models
+{
+    public class ModelList<T> : BaseModel
+    {
+        [Required(ErrorMessageResourceType = typeof(I18n.Roles), ErrorMessageResourceName = "ErrorNameRequired")]
+        public List<T> List { get; set; }
+    }
+}
---
+++ Dash/Models/ModelWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace Dash.Models
+{
+    public class ModelWrapper<T> : BaseModel
+    {
+        public T Model { get; set; }
+
+        public T GetModel()
+        {
+            return Model;
+        }
+    }
+}
