62ebb05cb7334bb84ba5d63fccc0268fd8898502
Chris Pittman <cpittman@gmail.com>
2018-04-30 20:41:30 -0400

    Remove unhelpful comments.

--- Dash/Scripts/Help.js
+++ Dash/Scripts/Help.js
@@ -7,10 +7,6 @@
     'use strict';
 
     var Help = {
-        /**
-         * Initialize the component.
-         * @param {Object} vnode - Mithril virtual node
-         */
         oninit: function(vnode) {
             var attrs = vnode.attrs;
             this.opts = {
@@ -19,19 +15,10 @@
             };
         },
 
-        /**
-         * Show help dialog.
-         * @param {Event} e - Event that triggered the dialog to open.
-         */
         showHelp: function(e) {
             Alertify.alert(this.opts.message, $.dialogs.focusOnClose.bind(e), $.dialogs.focusOnClose.bind(e));
         },
 
-        /**
-         * Create HTML to display content with help add on.
-         * @param {Object} vnode - Mithril virtual node
-         * @returns {Object} Mithril vnode
-         */
         view: function(vnode) {
             if (!this.opts.enabled) {
                 return $.isArray(vnode.children) ? m('.input-group', vnode.children) : vnode.children;
