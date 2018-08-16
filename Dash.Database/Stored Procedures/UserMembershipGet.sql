5900e5addf40df0fb92c3515e736067b4e1f855b
Chris Pittman <cpittman@gmail.com>
2018-08-15 20:06:30 -0400

    Add languageCode to resultset.

--- Dash.Database/Stored Procedures/UserMembershipGet.sql
+++ Dash.Database/Stored Procedures/UserMembershipGet.sql
@@ -4,7 +4,8 @@
  AS
 	SET NOCOUNT ON
 
-	SELECT Id, UserName, FirstName, LastName, LanguageId, [Password], Salt, LoginAttempts, DateUnlocks, AllowSingleFactor, Email, DateReset, ResetHash, DateLoginWindow, LoginHash
-    FROM [User] 
-    WHERE ((@UserName IS NOT NULL AND UserName = @UserName ) OR (@Email IS NOT NULL AND Email = @Email ))
-        AND [Status] = 1
+    SELECT u.Id, u.UserName, u.FirstName, u.LastName, u.LanguageId, u.[Password], u.Salt, u.LoginAttempts, u.DateUnlocks, u.AllowSingleFactor, u.Email, u.DateReset, u.ResetHash, u.DateLoginWindow, u.LoginHash, l.LanguageCode
+    FROM [User] u
+    INNER JOIN [Language] l ON l.Id = u.LanguageId
+    WHERE ((@UserName IS NOT NULL AND u.UserName = @UserName ) OR (@Email IS NOT NULL AND u.Email = @Email ))
+        AND u.[Status] = 1
