aee1e6441c4a51c7bb1d22dc9959ebd1de0930c7
Chris Pittman <cpittman@gmail.com>
2018-08-28 21:29:44 -0400

    Reformat file and add filter reordering.

--- Dash.Database/Stored Procedures/ReportFilterDelete.sql
+++ Dash.Database/Stored Procedures/ReportFilterDelete.sql
@@ -1,25 +1,28 @@
 ﻿CREATE PROCEDURE ReportFilterDelete
-	@Id INT,
-	@RequestUserId INT = NULL
+    @Id INT,
+    @RequestUserId INT = NULL
 AS
-	SET NOCOUNT ON
+    SET NOCOUNT ON
 
-	BEGIN TRY
-		BEGIN TRAN
-			UPDATE r SET DateUpdated = SYSDATETIMEOFFSET(), UserUpdated = @RequestUserId
-			FROM Report r
-			INNER JOIN ReportFilter rf ON rf.ReportId = r.Id
-			WHERE rf.Id = @Id
-
-			DELETE FROM ReportFilter WHERE Id = @Id
-		COMMIT
-		RETURN 0
-	END TRY
-	BEGIN CATCH
-		IF XACT_STATE() = -1 ROLLBACK
-		DECLARE @ErrorMessage NVARCHAR(4000), @ErrorSeverity INT, @ErrorState INT
-		SET @ErrorMessage = CAST (ERROR_NUMBER() AS VARCHAR) + ': ' + ERROR_MESSAGE()
-		SET @ErrorSeverity = ERROR_SEVERITY()
-		SET @ErrorState = ERROR_STATE()
-		RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState)
-	END CATCH
+    BEGIN TRY
+        BEGIN TRAN
+            UPDATE r SET DateUpdated = SYSDATETIMEOFFSET(), UserUpdated = @RequestUserId
+                FROM Report r
+                INNER JOIN ReportFilter rf ON rf.ReportId = r.Id
+                WHERE rf.Id = @Id
+            DECLARE @ReportId INT
+            DECLARE @DisplayOrder INT
+            SELECT TOP 1 @ReportId = ReportId, @DisplayOrder = DisplayOrder FROM ReportFilter WHERE Id = @Id
+            DELETE FROM ReportFilter WHERE Id = @Id
+            UPDATE ReportFilter SET DisplayOrder = DisplayOrder - 1 WHERE ReportId = @ReportId AND DisplayOrder > @DisplayOrder
+        COMMIT
+        RETURN 0
+    END TRY
+    BEGIN CATCH
+        IF XACT_STATE() = -1 ROLLBACK
+        DECLARE @ErrorMessage NVARCHAR(4000), @ErrorSeverity INT, @ErrorState INT
+        SET @ErrorMessage = CAST (ERROR_NUMBER() AS VARCHAR) + ': ' + ERROR_MESSAGE()
+        SET @ErrorSeverity = ERROR_SEVERITY()
+        SET @ErrorState = ERROR_STATE()
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState)
+    END CATCH
