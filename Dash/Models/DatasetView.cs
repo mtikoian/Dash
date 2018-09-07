0b434aac8acec537d28cda3c71581aea5f872438
Chris Pittman <cpittman@gmail.com>
2018-09-06 20:11:01 -0400

    Simplify dataset views.

---
+++ Dash/Models/DatasetView.cs
@@ -0,0 +1,23 @@
+﻿namespace Dash.Models
+{
+    public enum DatasetViewTab
+    {
+        Overview,
+        Joins,
+        Columns
+    }
+
+    public class DatasetView
+    {
+        public DatasetView(int id, bool isCreate, DatasetViewTab activeTab)
+        {
+            Id = id;
+            IsCreate = isCreate;
+            ActiveTab = activeTab;
+        }
+
+        public DatasetViewTab ActiveTab { get; set; }
+        public int Id { get; set; }
+        public bool IsCreate { get; set; }
+    }
+}
