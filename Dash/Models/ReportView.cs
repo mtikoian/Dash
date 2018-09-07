1461833ecd72eef76c04eb3edd8c09a7ed5e6b6c
Chris Pittman <cpittman@gmail.com>
2018-09-06 20:12:19 -0400

    Simplify report views.

---
+++ Dash/Models/ReportView.cs
@@ -0,0 +1,28 @@
+﻿namespace Dash.Models
+{
+    public enum ReportViewTab
+    {
+        Edit,
+        Filters,
+        Groups,
+        Columns,
+        Rename,
+        Share,
+        Export,
+        Sql
+    }
+
+    public class ReportView
+    {
+        public ReportView(int id, bool isOwner, ReportViewTab activeTab)
+        {
+            Id = id;
+            IsOwner = isOwner;
+            ActiveTab = activeTab;
+        }
+
+        public ReportViewTab ActiveTab { get; set; }
+        public int Id { get; set; }
+        public bool IsOwner { get; set; }
+    }
+}
