3f6129a05cb2f64d2d3207b2c17454d30a2160eb
Chris Pittman <cpittman@gmail.com>
2018-10-20 22:40:04 -0400

    issue #6 Add tag helper for form help.

---
+++ Dash/TagHelpers/FormHelp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Dash.TagHelpers
+{
+    public class FormHelpTagHelper : FormBaseTagHelper
+    {
+        public FormHelpTagHelper(IHtmlHelper htmlHelper) : base(htmlHelper)
+        {
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            Contextualize();
+
+            output.TagName = null;
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.PostContent.AppendHtml(BuildHelp());
+
+            base.Process(context, output);
+        }
+    }
+}
