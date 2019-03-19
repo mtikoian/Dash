623c4980300c44057211f2d3f88518fdcfa06854
Chris Pittman <cpittman@gmail.com>
2019-03-19 17:47:15 -0400

    Add new attribute to simplify model validation.

---
+++ Dash/Utils/ValidModelAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Dash.Models;
+using Dash.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Dash
+{
+    public sealed class ValidModelAttribute : ActionFilterAttribute
+    {
+        readonly bool UseTempData;
+
+        public ValidModelAttribute(bool useTempData = false) => UseTempData = useTempData;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var param = context.ActionArguments.FirstOrDefault(p => p.Value is BaseModel);
+            if (param.Value == null)
+                context.ModelState.AddModelError("general", Core.ErrorGeneric);
+
+            if (!context.ModelState.IsValid)
+            {
+                var controller = (Controller)context.Controller;
+                if (UseTempData)
+                    controller.TempData["Error"] = context.ModelState.ToErrorString();
+                else
+                    controller.ViewBag.Error = context.ModelState.ToErrorString();
+            }
+        }
+    }
+}
